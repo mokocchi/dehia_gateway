f649af7e44e9cda056d4a5e958ba2e3159f03e9e
Jose Arcidiacono <j2arcidiacono@outlook.com>
2021-04-20 09:07:23 -0300

    Servicio results

--- app/routers/resultsService.js
+++ app/routers/resultsService.js
@@ -4,10 +4,10 @@ const {postApi, getApi} = require('./utils')
 const apiAdapter = require('./apiAdapter')
 const apiPrefix = process.env.API_PREFIX;
 
-const api = apiAdapter(process.env.COLLECT_BASE_URL)
+const api = apiAdapter(process.env.RESULTS_BASE_URL)
 
-router.post(apiPrefix + "/results", (req, res) => {
-    postApi(api, req, res);
+router.get(apiPrefix + "/resultados", (req, res) => {
+    getApi(api, req, res);
 })
 
 module.exports = router
--- app/routers/router.js
+++ app/routers/router.js
@@ -4,14 +4,23 @@ var router = express.Router()
 var authRouter = require('./authService')
 var defineRouter = require('./defineService')
 var collectRouter = require('./collectService')
+var resultsRouter = require('./resultsService')
 
 router.use((req, res, next) => {
     console.log(`Called: ${req.method} ${req.path}`)
     next()
 })
 
+router.use(function(req, res, next){
+    res.on('finish', function(){
+      console.log("Status: " + res.statusCode);
+    });
+    next();
+  }); 
+
 router.use(authRouter)
 router.use(defineRouter)
 router.use(collectRouter)
+router.use(resultsRouter)
 
 module.exports = router
